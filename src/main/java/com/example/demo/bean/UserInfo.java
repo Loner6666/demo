e1aa4bfce52a23c81dc1b558ce61181e64edde45
GuoFeng <1365879071@qq.com>
2021-03-31 16:21:50 +0800

    javadoc注释

--- src/main/java/com/example/demo/bean/UserInfo.java
+++ src/main/java/com/example/demo/bean/UserInfo.java
@@ -15,23 +15,50 @@ import lombok.NoArgsConstructor;
 @ApiModel(value = "user对象", description = "用户对象user")
 public class UserInfo {
 
+    /**
+     * 编号
+     */
     @ApiModelProperty(value = "用户id", name = "id", example = "4")
-    private Long id;//编号
+    private Long id;
+    /**
+     * 用户名称
+     */
     @ApiModelProperty(value = "用户名称", name = "loginName", example = "用户4")
-    private String loginName;//用户名称
+    private String loginName;
+    /**
+     * 用户昵称
+     */
     @ApiModelProperty(value = "用户昵称", name = "nickName", example = "昵称4")
-    private String nickName;//用户昵称
+    private String nickName;
+    /**
+     * 用户密码
+     */
     @ApiModelProperty(value = "用户密码", name = "passwd", example = "202cb962ac59075b964b07152d234b70")
-    private String passwd;//用户密码
+    private String passwd;
+    /**
+     * 用户姓名
+     */
     @ApiModelProperty(value = "用户姓名", name = "name", example = "张三004")
-    private String name;//用户姓名
+    private String name;
+    /**
+     * 手机号
+     */
     @ApiModelProperty(value = "手机号", name = "phoneNum", example = "17300001114")
-    private String phoneNum;//手机号
+    private String phoneNum;
+    /**
+     * 邮箱
+     */
     @ApiModelProperty(value = "邮箱", name = "email", example = "17300001114@qq.com")
-    private String email;//邮箱
+    private String email;
+    /**
+     * 头像
+     */
     @ApiModelProperty(value = "头像", name = "headImg", example = "4")
-    private String headImg;//头像
+    private String headImg;
+    /**
+     * 用户级别
+     */
     @ApiModelProperty(value = "用户级别", name = "userLevel", example = "4")
-    private String userLevel;//用户级别
+    private String userLevel;
 
 }
